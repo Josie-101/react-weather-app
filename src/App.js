1c26f0724be9fa3d82b106279c6c23343c2b03f3
Josie-101 <86954364+Josie-101@users.noreply.github.com>
2021-12-04 14:05:12 +1100

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This app was coded by Jo B and is open sourced on {" "}
+        <a
+          href="https://github.com/Josie-101/react-weather-app"
+          target="blank"
+          norefferer
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 
