13212fc1bd904ccc1c99133ba70cd2c343aed4f4
Josie-101 <86954364+Josie-101@users.noreply.github.com>
2021-12-05 16:46:47 +1100

    Updated syntax

--- src/App.js
+++ src/App.js
@@ -9,20 +9,19 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-       <Weather defaultCity="Paris"/>
-      <footer>
-        This app was coded by Jo B and is open sourced on {" "}
-        <a
-          href="https://github.com/Josie-101/react-weather-app"
-          target="blank"
-          norefferer
-        >
-          Github
-        </a>
-      </footer>
+        <Weather defaultCity="Paris" />
+        <footer>
+          This app was coded by Jo B and is open sourced on{" "}
+          <a
+            href="https://github.com/Josie-101/react-weather-app"
+            target="blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
       </div>
     </div>
-  
   );
 }
 
